200dbc0c8881662cd1b41f136d9f32c6a100fb5d
haimbilia <42009428+haimbilia@users.noreply.github.com>
2022-07-14 20:42:15 +0300

    Update DeviceListerForm.cs

--- DeviceLister/DeviceLister/DeviceListerForm.cs
+++ DeviceLister/DeviceLister/DeviceListerForm.cs
@@ -1,5 +1,8 @@
-﻿using System;
+using System;
+using System.IO;
+using System.Reflection;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
@@ -7,6 +10,8 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using Microsoft.DirectX.DirectInput;
+
+
 
 namespace DeviceLister
 {
@@ -16,18 +21,69 @@ namespace DeviceLister
         {
             InitializeComponent();
         }
-
         private void DeviceListerForm_Load(object sender, EventArgs e)
         {
+            
+            
             string deviceData = "";
+            string devEnum = "";
+            int devnum = 0;
 
+            List<string> list = new List<string>();
+            IDictionary<string, int> NameNum = new Dictionary<string, int>();
             foreach (DeviceInstance di in Manager.GetDevices(DeviceClass.GameControl, EnumDevicesFlags.AttachedOnly))
             {
-                deviceData += "\"" + di.ProductName + "\": {" + di.InstanceGuid + "}" + System.Environment.NewLine;
+                
+                list.Add(di.ProductName);
+
             }
 
-            textBox.Text = deviceData;
-            textBox.Select(0, 0);
+            String[] str = list.ToArray();
+
+            var duplicates = str.GroupBy(x => x)
+            .Where(g => g.Count() > 1)
+            .ToDictionary(x => x.Key, y => y.Count());
+
+            
+            foreach (DeviceInstance di in Manager.GetDevices(DeviceClass.GameControl, EnumDevicesFlags.AttachedOnly))
+            {
+
+                if (NameNum.ContainsKey("" + di.ProductName + ""))
+                {
+                    devnum = NameNum["" + di.ProductName + ""];
+                    if (duplicates["" + di.ProductName + ""] > 1)
+                    {
+                        int newdevnum = devnum + 1;
+                        NameNum["" + di.ProductName + ""] = newdevnum;
+                        devEnum = "(" + newdevnum + ")";
+                    }
+                }
+                else
+                {
+                     NameNum.Add("" + di.ProductName + "", 1);
+                    if (duplicates.ContainsKey("" + di.ProductName + ""))
+                    {
+                        devEnum = "(1)";
+                    }
+                    else
+                    {
+                        devEnum = "";
+                    }
+                    
+                    
+                }
+
+
+                deviceData += "\"" + di.ProductName + "\"" + devEnum + "={" + di.InstanceGuid + "}" + System.Environment.NewLine;
+               
+
+            }
+
+             textBox.Text = deviceData;
+             textBox.Select(0, 0);
         }
+        
     }
+    
 }
+
